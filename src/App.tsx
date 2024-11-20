bb59b20523724595789e58a09c81f74a3af07d13
Agajan Sahatov <agajan.st@gmail.com>
2024-11-20 15:42:16 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+	return (
+		<>
+			<div className="app">
+				<h1>ANTD</h1>
+				<h2>Ant Design Playgound</h2>
+			</div>
+		</>
+	);
+}
+
+export default App;
