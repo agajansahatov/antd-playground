8e0a42a2e5ff6192c4205f380324cd664d344db3
Agajan Sahatov <agajan.st@gmail.com>
2024-11-20 15:51:21 +0800

    Test Button Component of ANTD

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
+import { useState } from "react";
 import "./App.css";
+import { Button } from "antd";
 
 function App() {
+	const [indicator, setIndicator] = useState(1);
+
 	return (
 		<>
 			<div className="app">
-				<h1>ANTD</h1>
-				<h2>Ant Design Playgound</h2>
+				<h1>Indicator: {indicator}</h1>
+				<Button
+					type="primary"
+					size="large"
+					onClick={() => setIndicator(indicator + 1)}
+				>
+					Increase
+				</Button>
 			</div>
 		</>
 	);
